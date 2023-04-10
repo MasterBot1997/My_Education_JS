116becb8315c38e20c6bbb9043f3c5fd7fe6f450
MasterBot1997 <bel3187228@yandex.ru>
2023-04-10 16:27:14 +0300

    Create lib.java

---
+++ java_project/lib.java
@@ -0,0 +1,16 @@
+package java_project;
+
+import java.util.Scanner;
+
+public class lib {
+    static void sayHi(){
+        System.out.println("hi");
+    }
+    static int data(){
+        Scanner iScanner = new Scanner(System.in);
+        System.out.printf("Number: ");
+        Integer number = iScanner.nextInt();
+        return number;
+    }
+}
+
