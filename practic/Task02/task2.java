7fcee2243f576fddc709271b339d011d30d84084
MasterBot1997 <bel3187228@yandex.ru>
2023-04-10 18:28:55 +0300

    Update task2.java

--- practic/Task02/task2.java
+++ practic/Task02/task2.java
@@ -2,6 +2,19 @@ package Task02;
 
 public class task2 {
     public static void main(String[] args) {
-        System.err.println(lib.data());
+        int[] arr = { 1, 1, 0, 1, 1, 1 };
+        int max = 0;
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 1) {
+                count += 1;
+                if (count > max) {
+                    max = count;
+                }
+            } else {
+                count = 0;
+            }
+        }
+        System.out.println(max);
     }
 }
