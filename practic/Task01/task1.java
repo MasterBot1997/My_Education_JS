0fd331d0e9943de4d0a207c17c1d9ad56a8d00e5
MasterBot1997 <bel3187228@yandex.ru>
2023-04-10 17:09:01 +0300

    Create task1.java

---
+++ practic/Task01/task1.java
@@ -0,0 +1,14 @@
+package Task01;
+
+// import java.util.Scanner;
+
+public class task1 {
+    public static void main(String[] args) {
+        int a = lib.data(), b = lib.data();
+        System.err.println(a + " + " + b + " = " + lib.sum(a, b));
+        // Scanner sc = new Scanner(System.in);
+        // String name = sc.nextLine();
+        // System.out.println(name);
+        // sc.close();
+    }       
+}       
