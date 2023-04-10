aed77d60d9fabc156d34cf87e0f10acb1bf420af
MasterBot1997 <bel3187228@yandex.ru>
2023-04-10 16:12:49 +0300

    Create Program.java

---
+++ Practic/p1/Task01/Program.java
@@ -0,0 +1,10 @@
+package Practic.p1.Task01;
+
+public class Program {
+    public static void main(String[] args) {
+        int a = 1, b = 2;
+        int c = a + b;
+        String res = a + " + " + b + " = " + c;
+        System.out.println(res);
+    }
+}
