3faddc29db9c777f5b7d5da867e48d597071c75d
MasterBot1997 <bel3187228@yandex.ru>
2023-04-13 16:20:03 +0300

    Create calcOp.java

---
+++ houmWork/h1/Task03/calcOp.java
@@ -0,0 +1,23 @@
+package Task03;
+
+public class calcOp {
+    static int sum(int a, int b) {
+        int c = a + b;
+        return c;
+    }
+
+    static int dif(int a, int b) {
+        int c = a - b;
+        return c;
+    }
+
+    static int mult(int a, int b) {
+        int c = a * b;
+        return c;
+    }
+
+    static int div(int a, int k) {
+        int c = a / k;
+        return c;
+    }
+}
