a5d8503c837e6a330740d426fa43a2a8572c37f0
MasterBot1997 <bel3187228@yandex.ru>
2023-04-13 15:50:23 +0300

    Update task02.java

--- houmWork/h1/Task02/task02.java
+++ houmWork/h1/Task02/task02.java
@@ -3,5 +3,19 @@ package Task02;
 // Вывести все простые числа от 1 до 1000
 
 public class task02 {
-    
+    public static void main(String[] args) {
+        System.out.printf("%d ", 1);
+        for (int i = 2; i <= 1000; i++) {
+            boolean a = true;
+            for (int j = 2; j < i; j++) {
+                if (i % j == 0) {
+                    a = false;
+                    break;
+                }
+            }
+            if (a) {
+                System.out.printf("%d ", i);
+            }
+        }
+    }
 }
