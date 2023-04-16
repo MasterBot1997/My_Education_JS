1c995986f5a73c53c4f234539ea503ce6f54b6ca
MasterBot1997 <bel3187228@yandex.ru>
2023-04-16 19:22:36 +0300

    Update task03.java

--- houmWork/h1/Task03/task03.java
+++ houmWork/h1/Task03/task03.java
@@ -1,6 +1,10 @@
 package Task03;
 
+import java.io.IOException;
 import java.util.Scanner;
+import java.util.logging.FileHandler;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 
 // Реализовать простой калькулятор (+ - / *)
 // Ввод числа ->
@@ -8,7 +12,13 @@ import java.util.Scanner;
 // Ввод числа ->
 
 public class task03 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SecurityException, IOException {
+        Logger logger = Logger.getLogger(Task03.class.getName());
+        FileHandler fh = new FileHandler("log.txt");
+        logger.addHandler(fh);
+        SimpleFormatter sFormat = new SimpleFormatter();
+        fh.setFormatter(sFormat);
+
         Scanner sc = new Scanner(System.in);
         System.out.println("Введите первое число: ");
         int num1 = sc.nextInt();
@@ -18,13 +28,14 @@ public class task03 {
         String simb = sc.next();
         switch (simb) {
             case "+":
+                logger.info(num1 + " " + simb + " " + num2 + " = " + calcOp.sum(num1, num2));
                 System.out.printf("%d %s %d = %d", num1, simb, num2, calcOp.sum(num1, num2));
                 break;
             case "-":
-                System.out.printf("%d %s %d = %d", num1, simb, num2, calcOp.sum(num1, num2));
+                System.out.printf("%d %s %d = %d", num1, simb, num2, calcOp.dif(num1, num2));
                 break;
             case "*":
-                System.out.printf("%d %s %d = %d", num1, simb, num2, calcOp.sum(num1, num2));
+                System.out.printf("%d %s %d = %d", num1, simb, num2, calcOp.mult(num1, num2));
                 break;
             case "/":
                 if (num2 != 0) {
