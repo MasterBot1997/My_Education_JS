c7a3b9acbe191791e55e1aff84c9a793a94638d5
MasterBot1997 <bel3187228@yandex.ru>
2023-04-16 18:32:17 +0300

    Update Task02.java

--- houmWork/h2/Task02/Task02.java
+++ houmWork/h2/Task02/Task02.java
@@ -1,5 +1,11 @@
 package houmWork.h2.Task02;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Scanner;
+
 // 2. Дана строка (получение через обычный текстовый файл!!!)
 
 // "фамилия":"Иванов","оценка":"5","предмет":"Математика"
@@ -13,10 +19,50 @@ package houmWork.h2.Task02;
 // Студент Петрова получил 4 по предмету Информатика.
 // Студент Краснов получил 5 по предмету Физика.
 
-// 3. дополнительно. К калькулятору из предыдущего дз добавить логирование.
-// 1 + 3 = 4
-// 4 + 3 = 7
-
 public class Task02 {
-    
+    public static void main(String[] args) throws FileNotFoundException {
+        String pathProject = System.getProperty("user.dir");
+        String pathFile = pathProject.concat("/file.txt");
+        File file = new File(pathFile);
+
+        Scanner sc = new Scanner(file);
+        while (sc.hasNextLine()) {
+            String data = Task02.newL(sc);
+            // System.out.println(data);
+            // Task02.printData(data);
+            System.out.println(Task02.printData(data));
+        }
+        // String data = Task02.newL(sc);
+        // System.out.println(data);
+
+        // Task02.printData(data);
+
+        sc.close();
+    }
+
+    // Через StringBuilder сплитим все значения без знаков и возвращаем строку
+    static String newL(Scanner scann) {
+        String line = scann.nextLine();
+        String[] words = line.split(",");
+        StringBuilder newLine = new StringBuilder();
+        for (int i = 0; i < words.length; i++) {
+            for (int j = 0; j < words[i].split(":").length; j++) {
+                newLine.append(words[i].split(":")[j] + " ");
+            }
+        }
+
+        return newLine.toString();
+    }
+
+    // Метод вывода результата в консоль 
+    // static void printData(String da) {
+    //     System.out.println(
+    //             "Студент " + da.split(" ")[1] + " получил " + da.split(" ")[3] + " по предмету " + da.split(" ")[5]);
+    // }
+
+    static String printData(String da){
+        StringBuilder printDa = new StringBuilder();
+        printDa.append("Студент " + da.split(" ")[1] + " получил " + da.split(" ")[3] + " по предмету " + da.split(" ")[5]);
+        return printDa.toString();
+    }
 }
