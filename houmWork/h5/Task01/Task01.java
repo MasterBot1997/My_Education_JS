eae5fac5806335ee427b04b95b76105feb9dd70b
Ivan Belyaev <bel3187228@yandex.ru>
2023-04-25 19:00:54 +0300

    Update Task01.java

--- houmWork/h5/Task01/Task01.java
+++ houmWork/h5/Task01/Task01.java
@@ -1,5 +1,9 @@
 package houmWork.h5.Task01;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 // Реализуйте структуру телефонной книги с помощью HashMap, учитывая, что 1 человек может иметь несколько телефонов.
 // Добавить функции:
 // 1) Добавление номера
@@ -25,6 +29,45 @@ package houmWork.h5.Task01;
 
 public class Task01 {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        Map<String, String> telephones = new HashMap<>();
+        System.out.println("Список комманд:\n1 - Добавление номера\n2 - Вывод всего\nstop - остановка программы");
+        System.out.print("Введите команду которую хотите выполнить: ");
+        String command = sc.nextLine();
+
+        while (!command.equals("stop")) {
+            switch (command) {
+                case "1":
+                    telephones = newNumberphone(sc, telephones);
+                    break;
+                case "2":
+                    System.out.println(telephones);
+                    break;
+
+                default:
+                    break;
+            }
+            System.out.print("Введите команду которую хотите выполнить: ");
+            command = sc.next();
+        }
+
+        sc.close();
     }
+
+    static Map<String, String> newNumberphone(Scanner scan, Map<String, String> newTelephones) {
+        System.out.print("Введите фамилию: ");
+        String lastName = scan.next();
+        System.out.print("Введите номер: ");
+        String numberPhone = scan.next();
+        if (newTelephones.containsKey(lastName)) {
+            newTelephones.put(lastName, newTelephones.get(lastName) + " " + numberPhone);
+            // System.out.println(newTelephones.values());
+        } else {
+            System.out.println("2");
+            newTelephones.put(lastName, numberPhone);
+        }
+        return newTelephones;
+    }
+
+    
 }
