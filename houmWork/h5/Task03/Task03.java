17cc42aee3c9595af75177cdce825168cfc95443
Ivan Belyaev <bel3187228@yandex.ru>
2023-04-26 20:23:37 +0300

    Update Task03.java

--- houmWork/h5/Task03/Task03.java
+++ houmWork/h5/Task03/Task03.java
@@ -1,5 +1,8 @@
 package houmWork.h5.Task03;
 
+import java.util.Arrays;
+// import java.util.Random;
+
 // На шахматной доске расставить 8 ферзей так, чтобы они не били друг друга. И вывести Доску. Пример вывода доски 8x8
 // 0x000000
 // 0000x000
@@ -7,6 +10,44 @@ package houmWork.h5.Task03;
 
 public class Task03 {
     public static void main(String[] args) {
-        
+        String[][] table = new String[8][8];
+        // Random rnd = new Random();
+        // int firstMoveI = rnd.nextInt(8);
+        // int firstMoveJ = rnd.nextInt(8);
+
+        int firstMoveI = 4;
+        int firstMoveJ = 5;
+
+        // Два цикла создания и печати доски c первым ферзем
+        for (int i = 0; i < table.length; i++) {
+            for (int j = 0; j < table.length; j++) {
+                if (i == firstMoveI && j == firstMoveJ) {
+                    table[i][j] = "X";
+                } else {
+                    table[i][j] = "0";
+                }
+
+            }
+        }
+
+        for (int i = 0; i < table.length; i++) {
+            System.out.println(Arrays.deepToString(table[i]));
+        }
+    }
+
+
+    // j -колонна
+    // i - строка
+    static boolean check(String[][] tableCheck){
+        int indexI = 0;
+        int indexJ = 0;
+        for (int i = 0; i < tableCheck.length; i++) {
+            for (int j = 0; j < tableCheck.length; j++) {
+                if (i == 0){
+                    
+                }
+            }
+        }
+        return true;
     }
 }
