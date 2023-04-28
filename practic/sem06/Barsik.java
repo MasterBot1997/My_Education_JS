9ac416ab4c1685a75ce81ef11626ecbcfe899c34
Ivan Belyaev <bel3187228@yandex.ru>
2023-04-28 19:23:41 +0300

    Create Barsik.java

---
+++ practic/sem06/Barsik.java
@@ -0,0 +1,17 @@
+package sem06;
+
+public class Barsik {
+    
+    public long birthday;
+    public boolean pol;
+    public String nik;
+    public String kind;
+    public int id_kot;
+    public boolean injection;
+
+    public boolean injectionKot(){
+        if (injection) return true;
+        return false;
+    }
+
+}
